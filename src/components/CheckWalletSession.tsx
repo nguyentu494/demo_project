3f8ceb51658d35f964e917c467846f829ede5a11
nguyentu494 <nguyentu2003.khtgdd@gmail.com>
2025-10-10 17:45:21 +0700

    bug: disconnected v2

--- src/components/CheckWalletSession.tsx
+++ src/components/CheckWalletSession.tsx
@@ -3,21 +3,27 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAccount, useDisconnect } from "wagmi";
+import { config } from "../../config";
 
 export function WalletSessionSync() {
   const router = useRouter();
   const { isConnected } = useAccount();
-  const { disconnect, reset } = useDisconnect({});
+  const { disconnect, reset, isError, isPending } = useDisconnect({config: config, mutation: { onError(error) {
+    console.error("Disconnect error:", error);
+  }}});
 
   useEffect(() => {
     const handler = async () => {
       const hasToken = document.cookie.includes("accessToken=");
       if (!hasToken && isConnected) {
         try {
-          disconnect;
-          reset();
+          disconnect();
+          
+          // reset();
         } catch (err) {
           console.error("Error during disconnect:", err);
+        } finally {
+          // router.push("/login");
         }
       }
       if (isConnected) return;
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect } from "react";
 
 import { getBalance } from "wagmi/actions";
